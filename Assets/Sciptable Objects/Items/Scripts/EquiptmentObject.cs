d17682101104085cb8d611a9d1a5ff99350f8d6b
Wyatt <Wyattjarrell@yahoo.com>
2021-04-29 19:25:31 -0500

    Inventory System

---
+++ Assets/Sciptable Objects/Items/Scripts/EquiptmentObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Equiptment Object", menuName = "Inventory System/Items/Equiptment")]
+public class EquiptmentObject : ItemObjects
+{
+    public float atkBonus;
+    public float defenceBonus;
+    public void Awake()
+    {
+        type = ItemType.Equiptment;
+    }
+}
